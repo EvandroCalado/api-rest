b0921c48c26d0cb1e317618d431ef16de9286e38
Evandro Calado <evandrocalado07@gmail.com>
2023-12-08 00:06:47 -0300

    ✨ feat(models): create user

---
+++ src/models/User.js
@@ -0,0 +1,56 @@
+import bcryptjs from 'bcryptjs';
+import { Model, Sequelize } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'O nome deve ter entre 3 e 255 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Email já existente',
+          },
+          validate: {
+            isEmail: {
+              msg: 'E-mail inválido',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'A senha deve ter entre 6 e 50 caracteres',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
