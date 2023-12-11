cee8bb806d5de1dfcadbb7bbe7562671d2e74e23
Evandro Calado <evandrocalado07@gmail.com>
2023-12-11 09:21:21 -0300

    🐳 chore(controller): add try catch

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import Foto from '../models/Foto';
 
 const upload = multer(multerConfig).single('foto');
 
@@ -12,7 +13,21 @@ class FotoController {
         });
       }
 
-      return res.json(req.file);
+      try {
+        const { originalname, filename } = req.file;
+
+        const { aluno_id } = req.body;
+
+        const foto = await Foto.create({
+          originalname,
+          filename,
+          aluno_id,
+        });
+
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({ errors: ['Aluno ID não existe ou não informado'] });
+      }
     });
   }
 }
