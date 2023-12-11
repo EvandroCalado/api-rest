bc54b8f98c16e535c4f07dc237047fc43fbe829f
Evandro Calado <evandrocalado07@gmail.com>
2023-12-11 09:20:44 -0300

    🐳 chore(controller): add foto field

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,8 +1,16 @@
 import Aluno from '../models/Aluno';
+import Foto from '../models/Foto';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['filename', 'url'],
+      },
+    });
 
     return res.json(alunos);
   }
@@ -15,7 +23,14 @@ class AlunoController {
         return res.status(400).json({ errors: ['Faltando ID'] });
       }
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+        order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+        include: {
+          model: Foto,
+          attributes: ['filename', 'url'],
+        },
+      });
 
       if (!aluno) {
         return res.status(400).json({ errors: ['Aluno não existe'] });
