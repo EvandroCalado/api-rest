285ccfbd25f16ba3e9180b00f08e9d4b9eeb3c20
Evandro Calado <evandrocalado07@gmail.com>
2023-12-10 23:02:48 -0300

    🐳 chore(controller): change return

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Elaynne',
-      sobrenome: 'Cristina',
-      email: 'elaynne@gmail.com',
-      idade: 37,
-      peso: 70,
-      altura: 1.65,
-    });
-
-    res.json(novoAluno);
+    return res.json('index');
   }
 }
 
